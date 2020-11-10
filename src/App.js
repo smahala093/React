8c4b2a8e6e11d7df46c7c8850d93114bfd0f139d
smahala093 <74241566+smahala093@users.noreply.github.com>
2020-11-10 16:11:30 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+import "./styles.css";
+
+import Person from "./Person/Person";
+class App extends Component {
+  //export default function App() {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello Sandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        <p>this is working</p>
+        <Person />
+        <Person />
+        <Person />
+      </div>
+    );
+  }
+  // render() {
+  //   return React.createElement(
+  //     "div",
+  //     null,
+  //     React.createElement("h1", { className: "App" }, "Is does work now")
+  //   );
+  // }
+}
+export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const person = () => {
+  return <p>I am a person and my {Math.floor(Math.random() * 30)} age </p>;
+};
+
+export default person;
